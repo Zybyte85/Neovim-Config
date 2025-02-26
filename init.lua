eccd04af06367fee3a53c813bc52700ae2aa234f
Zybyte85 <cade.step01@gmail.com>
2025-02-25 17:04:17 -0700

    Small changes

--- init.lua
+++ init.lua
@@ -45,6 +45,16 @@ vim.api.nvim_create_autocmd('VimLeavePre', {
 
 -- LSP stuff
 require('mason').setup()
+require('mason-lspconfig').setup()
+
+require("mason-lspconfig").setup_handlers {
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        function (server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup {}
+        end,
+    }
 
 local cmp = require 'cmp'
 
